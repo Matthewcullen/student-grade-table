669b3eb94063fd9098dd9090c521e99a9a5567f8
matthew cullen <cullenwmatthew@gmail.com>
2020-05-06 17:02:19 -0700

    created methods on App Class

--- components/app.js
+++ components/app.js
@@ -1,5 +1,27 @@
 class App{
   constructor(){
-
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this)
+    this.handleGetGradesError = this.handleGetGradesError.bind(this)
+  }
+  handleGetGradesError(error){
+    console.log(error);
+  }
+  handleGetGradesSuccess(grades){
+    console.log(grades);
+  }
+  getGrades(){
+    $.ajax({
+      method: "GET",
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      data: "none",
+      headers:{
+        X - Access - Token:"xX0ypzyk"
+      },
+      success: handleGetGradesSuccess,
+      error: handleGetGradesError
+    })
+  }
+  Start(){
+    this.getGrades();
   }
 }
